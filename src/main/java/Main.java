f84dc1083ef7cb150f0b8ce14566312b187fa4ee
vadim novitskyi <thrownovitskyi@gmail.com>
2023-04-17 15:07:46 +0300

    fixed bug

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 
 import java.sql.*;
-//import org.h2.Driver;
+import org.h2.Driver;
 
 public class Main {
     private static String URL = "jdbc:h2:D:/H2/dv_3_DV";
@@ -9,9 +9,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        Driver driver = new Driver();
+        Driver driver = new Driver();
         try ( Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);) {
-//            DriverManager.registerDriver(driver);
+            DriverManager.registerDriver(driver);
             connection.isReadOnly();
         } catch (SQLException e) {
             throw new RuntimeException(e);
