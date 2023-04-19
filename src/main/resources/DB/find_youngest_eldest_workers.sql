4a97677808b90c5a94f9492813f4604d5a5d0224
vadim novitskyi <thrownovitskyi@gmail.com>
2023-04-19 23:11:44 +0300

    new task 6

--- src/main/resources/DB/find_youngest_eldest_workers.sql
+++ src/main/resources/DB/find_youngest_eldest_workers.sql
@@ -1,11 +1,10 @@
 --task_6
 select name, birthday,
 case
- when (datediff(year,birthday,'2023-01-01')<=23) then 'YOUNGEST'
- when (datediff(year,birthday,'2023-01-01')>=40) then 'OLDEST'
+ when birthday = (SELECT MIN(birthday) FROM worker) then 'YOUNGEST'
+ when birthday = (SELECT MAX(birthday) FROM worker) then 'OLDEST'
   end as type
   from worker
 group by birthday
 
 having type in ('YOUNGEST' ,'OLDEST');
---having delete from worker datediff(year,birthday,'2023-01-01') is null;
